1a3613959bc3e003e730ff6e1e35b715c76c19b6
Eros <erosburastero_it@hotmail.com>
2022-03-31 18:46:37 -0300

    i've finished the responsive web

--- components/LargeCardDisplay.vue
+++ components/LargeCardDisplay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-16" >
-      <h4 class="black--text display-1 d-flex my-5  justify-center"> {{ cardsSection.title }}</h4>
-      <p  class="black--text subtitle d-flex justify-center"> {{ cardsSection.snippet }} </p>
+      <h4 class="display-1 my-5 d-flex justify-center"> {{ cardsSection.title }}</h4>
+      <p  class="subtitle d-flex justify-center"> {{ cardsSection.snippet }} </p>
       <div class="white d-flex justify-center flex-wrap">
           <LargeCard v-for="card in cardsSection.cards"
           :key="card.id"
