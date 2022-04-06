95f3cc37268234794b20cbd7fe08a79f8343df66
Eros <erosburastero_it@hotmail.com>
2022-04-05 22:16:23 -0300

    i deleted all the cypress example files and i created a locator file

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("Locators", () => {
+    beforeEach(() => {
+        cy.visit("/products")
+    })
+
+    it("locating elements with get", () => {
+
+    })
+})
