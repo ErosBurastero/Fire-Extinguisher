d67da1bea1fa4a36949ecaeed7b2d13dd13731d4
Eros <erosburastero_it@hotmail.com>
2022-04-21 21:21:25 -0300

    i updated the test to the new btn

--- cypress/integration/test/order.spec.js
+++ cypress/integration/test/order.spec.js
@@ -7,9 +7,6 @@ describe("order", () => {
 
     it("get products into my items", () => {
         cy.contains("START LOOKING")
-        cy.intercept("GET", "https://randomuser.me/api/?results=5", {
-            fixture: "users.json"
-        })
         expect("/products")
         cy.get('[href="/products/1"] > .images').should("be.visible").click()
         expect("/products/1")
@@ -18,7 +15,7 @@ describe("order", () => {
         cy.get('.v-btn').should("be.visible").click()
         cy.get(':nth-child(2) > :nth-child(5) > .v-btn > .v-btn__content').click()
         cy.get(':nth-child(3) > :nth-child(4) > .v-btn > .v-btn__content').click()
-        cy.contains("RENT").should("be.visible").click()
+        cy.get('.my-10 > .v-btn__content').click()
         cy.get('.v-main__wrap > .v-card')
         expect("My Items")
         cy.get('[href="/my-items"]').click()
